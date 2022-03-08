7befb7d1d4b616f070c72dbade2d6f51773fef78
Hardik Chapla <chaplahardik009@gmail.com>
2022-03-08 10:19:09 +0530

    update 08-03-22

--- video-detail.php
+++ video-detail.php
@@ -1,3 +1,10 @@
+<?php
+    include('inc/connection/connection.php');
+    include('inc/helper/constant.php');
+    ob_start();
+    session_start();
+    include('page-title.php');
+?>
 <!doctype html>
 <html lang="en">
 
@@ -16,95 +23,91 @@
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="stylesheet" href="css/toastr.css" type="text/css">
     <link rel="stylesheet" href="css/media_query.css" type="text/css">
-    <title> Video detail </title>
+    <title> <?= $titleName ?> </title>
 </head>
 <div class="container-fluid ss_padding_live_events">
-    <div class="">
-        <div class="row">
-            <div class="col-lg-12 ss_mobile_padd_0">
-                <div class="ss_live_video">
-                    <div class="video_images">
-                        <img src="images/slider_img.jpg" alt="video">
-                        <div class="ss_video_player">
-                            <div class="rSlider">
-                                <span class="slide"></span>
-                                <input id="range" type="range" min="0" max="50000">
-                            </div>
-                            <div class="d-flex bd-highlight justify-content-center">
-                                <div class="p-2 flex-fill bd-highlight">
-                                    <div class="d-flex bd-highlight ss_volume ">
-                                        <div class=" bd-highlight ss_volume_icons align-self-center"><a href="#"><i
-                                                    class="fa fa-volume-up" aria-hidden="true"></i></a></div>
-                                        <div class=" bd-highlight align-self-center">
-                                            <div class="rSlider ss_rslider">
-                                                <span class="Vslide"></span>
-                                                <input id="volume" type="range" min="0" max="50000">
-                                            </div>
+    <div class="row">
+        <div class="col-lg-12 ss_mobile_padd_0">
+            <div class="ss_live_video">
+                <div class="video_images">
+                    <img src="images/slider_img.jpg" alt="video">
+                    <div class="ss_video_player">
+                        <div class="rSlider">
+                            <span class="slide"></span>
+                            <input id="range" type="range" min="0" max="50000">
+                        </div>
+                        <div class="d-flex bd-highlight justify-content-center">
+                            <div class="p-2 flex-fill bd-highlight">
+                                <div class="d-flex bd-highlight ss_volume ">
+                                    <div class=" bd-highlight ss_volume_icons align-self-center"><a href="#"><i
+                                                class="fa fa-volume-up" aria-hidden="true"></i></a></div>
+                                    <div class=" bd-highlight align-self-center">
+                                        <div class="rSlider ss_rslider">
+                                            <span class="Vslide"></span>
+                                            <input id="volume" type="range" min="0" max="50000">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="p-2 flex-fill bd-highlight text-center">
-                                    <div class="ss_play_button">
-                                        <a href="#"><i class="fa fa-backward" aria-hidden="true"></i> </a>
-                                        <a href="#"> <i class="fa fa-pause" aria-hidden="true"></i> </a>
-                                        <a href="#"> <i class="fa fa-forward" aria-hidden="true"></i> </a>
-                                    </div>
+                            </div>
+                            <div class="p-2 flex-fill bd-highlight text-center">
+                                <div class="ss_play_button">
+                                    <a href="#"><i class="fa fa-backward" aria-hidden="true"></i> </a>
+                                    <a href="#"> <i class="fa fa-pause" aria-hidden="true"></i> </a>
+                                    <a href="#"> <i class="fa fa-forward" aria-hidden="true"></i> </a>
                                 </div>
-                                <div class="p-2 flex-fill bd-highlight text-right">
-                                    <p>12.45/28.32</p>
-                                </div>
+                            </div>
+                            <div class="p-2 flex-fill bd-highlight text-right">
+                                <p>12.45/28.32</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="ss_video_des ss_button_right pt-5">
+            </div>
+            <div class="ss_video_des ss_button_right pt-5">
 
-                    <div class="d-flex bd-highlight ss_mobiles_block">
-                        <div class="p-2 flex-fill bd-highlight align-self-center">
-                            <div class="model_title_images_store">
-                                <img src="images/cosmic_comedy.png" alt="images">
-                            </div>
+                <div class="d-flex bd-highlight ss_mobiles_block">
+                    <div class="p-2 flex-fill bd-highlight align-self-center">
+                        <div class="model_title_images_store">
+                            <img src="images/cosmic_comedy.png" alt="images">
                         </div>
-                        <div class="p-2 flex-fill bd-highlight align-self-center">
-                            <div class="modal_des">
-                                <div class="match">
-                                    <h5><span>2020</span></h5>
-                                    <h6>16+</h6>
-                                    <h5><span>2 Seasons</span></h5>
-                                </div>
-                                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                                    praesentium
-                                    voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
-                                    occaecati cupiditate non provident Years after they rose to fame as young
-                                    crime-fighting superheroes, the estranged Hargreeves sibling .com together to
-                                    make their father’s death </p>
+                    </div>
+                    <div class="p-2 flex-fill bd-highlight align-self-center">
+                        <div class="modal_des">
+                            <div class="match">
+                                <h5><span>2020</span></h5>
+                                <h6>16+</h6>
+                                <h5><span>2 Seasons</span></h5>
                             </div>
+                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                                praesentium
+                                voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+                                occaecati cupiditate non provident Years after they rose to fame as young
+                                crime-fighting superheroes, the estranged Hargreeves sibling .com together to
+                                make their father’s death </p>
                         </div>
+                    </div>
 
-                        <div class="p-2 flex-fill bd-highlight align-self-center">
+                    <div class="p-2 flex-fill bd-highlight align-self-center">
 
-                            <div class="tool_epi mb-4 mt-0">
-                                <div class="epi_t_box">
-                                    <div class="epi_tool_box">
-                                        <a href="#"> <img src="images/StashS_2x.png" alt="">
-                                        </a>
-                                    </div>
-                                    <a href="#">
+                        <div class="tool_epi mb-4 mt-0">
+                            <div class="epi_t_box">
+                                <div class="epi_tool_box">
+                                    <a href="#"> <img src="images/StashS_2x.png" alt="">
                                     </a>
-                                    <div class="epi_tool_box"><a href="#">
-                                        </a><a href="#"><img class="img-fluid" src="images/like.png"></a>
-                                    </div>
-                                    <div class="epi_tool_box">
-                                        <a href="#"><img class="img-fluid" src="images/dislike.png"></a>
-                                    </div>
-                                    <div class="epi_tool_box">
-                                        <a href="#"><i class="fa fa-volume-up" aria-hidden="true"></i></a>
-                                    </div>
+                                </div>
+                                <a href="#">
+                                </a>
+                                <div class="epi_tool_box"><a href="#">
+                                    </a><a href="#"><img class="img-fluid" src="images/like.png"></a>
+                                </div>
+                                <div class="epi_tool_box">
+                                    <a href="#"><img class="img-fluid" src="images/dislike.png"></a>
+                                </div>
+                                <div class="epi_tool_box">
+                                    <a href="#"><i class="fa fa-volume-up" aria-hidden="true"></i></a>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
                 </div>
             </div>
