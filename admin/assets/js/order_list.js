f053686dee672fff97112e1c700b1feded8100a3
Sanghani Chirag <csanghani58@gmail.com>
2022-03-22 23:06:03 +0530

    Updated 2022-03-22
    Changes on order list for changes status and no repeat status and send to the user mail

--- admin/assets/js/order_list.js
+++ admin/assets/js/order_list.js
@@ -1,49 +1,54 @@
-function showLoading() {
-  document.getElementById("page-loader").style = "visibility: visible";
+function showLoading1() {
+    document.getElementById("page-loader").style.visibility = "visible";
 }
 
-function hideLoading() {
-  document.getElementById("page-loader").style = "visibility: hidden";
+function hideLoading1() {
+    document.getElementById("page-loader").style.visibility = "hidden";
 }
 
-$(document).ready(function () {
-  var orderslist = $("#orderslist").DataTable({
-    dom: "Bfrtip",
-    buttons: [
-      { extend: "copyHtml5", exportOptions: { columns: [0, ":visible"] } },
-      { extend: "pdfHtml5", exportOptions: { columns: ":visible" } },
-      {
-        text: "JSON",
-        action: function (a, o, t, r) {
-          var e = o.buttons.exportData();
-          $.fn.dataTable.fileSave(new Blob([JSON.stringify(e)]), "Export.json");
+$(document).ready(function() {
+    var orderslist = $("#orderslist").DataTable({
+        dom: "Bfrtip",
+        buttons: [
+            { extend: "copyHtml5", exportOptions: { columns: [0, ":visible"] } },
+            { extend: "pdfHtml5", exportOptions: { columns: ":visible" } },
+            {
+                text: "JSON",
+                action: function(a, o, t, r) {
+                    var e = o.buttons.exportData();
+                    $.fn.dataTable.fileSave(new Blob([JSON.stringify(e)]), "Export.json");
+                },
+            },
+            { extend: "print", exportOptions: { columns: ":visible" } },
+        ],
+        ajax: {
+            url: "resources/display_orders",
+            type: "POST",
         },
-      },
-      { extend: "print", exportOptions: { columns: ":visible" } },
-    ],
-    ajax: {
-      url: "resources/display_orders",
-      type: "POST",
-    },
-  });
-  $(document).on("change", ".order_status", function (e) {
-    var order_id = $(this).attr("id");
-    var status = $(this).val();
-    $.ajax({
-      url: "resources/change_order_status",
-      type: "POST",
-      data: { order_id: order_id, status: status },
-      dataType: "json",
-    })
-      .done(function (response) {
-        if (response.success == "success") {
-          toastr.success(response.message).delay(1000).fadeOut(1000);
-        } else {
-          toastr.warning(output.message).delay(1000).fadeOut(1000);
-        }
-      })
-      .fail(function () {
-        swal("Oops...", "Something went wrong with ajax !", "error");
-      });
-  });
+    });
+    $(document).on("change", ".order_status", function(e) {
+        var order_id = $(this).attr("id");
+        var status = $(this).val();
+        showLoading1();
+        $.ajax({
+                url: "resources/change_order_status",
+                type: "POST",
+                data: { order_id: order_id, status: status },
+                dataType: "json",
+            })
+            .done(function(response) {
+                if (response.success == "success") {
+                    orderslist.ajax.reload();
+                    toastr.success(response.message).delay(1000).fadeOut(1000);
+                    hideLoading1();
+                } else {
+                    toastr.warning(output.message).delay(1000).fadeOut(1000);
+                    hideLoading1();
+                }
+            })
+            .fail(function() {
+                swal("Oops...", "Something went wrong with ajax !", "error");
+                hideLoading1();
+            });
+    });
 });
--- admin/resources/display_orders.php
+++ admin/resources/display_orders.php
@@ -20,34 +20,20 @@ foreach ($result as $row) {
     $sub_array[] = $row["final_amount"];
     $aa = '<select name="order_status" class="form-control order_status" id="'.$row['id'].'">';
     if($row['status'] == 0){
-        $aa .= '<option selected value="0">Panding</option>';
+        $aa .= '<option selected value="0">Panding</option><option value="1">Confirm</option><option value="2">Shipped</option><option value="3">Delivered</option><option value="4">Completed</option>';
+    } elseif ($row['status'] == 1) {
+        $aa .= '<option selected value="1">Confirm</option><option value="2">Shipped</option><option value="3">Delivered</option><option value="4">Completed</option>';
+    } elseif ($row['status'] == 2) {
+        $aa .= '<option selected value="2">Shipped</option><option value="3">Delivered</option><option value="4">Completed</option>';
+    } elseif ($row['status'] == 3) {
+        $aa .= '<option selected value="3">Delivered</option><option value="4">Completed</option>';
     } else {
-        $aa .= '<option value="0">Panding</option>';
-    }
-    if($row['status'] == 1){
-        $aa .= '<option selected value="1">Confirm</option>';
-    } else {
-        $aa .= '<option value="1">Confirm</option>';
-    }
-    if($row['status'] == 2){
-        $aa .= '<option selected value="2">Shipped</option>';
-    } else {
-        $aa .= '<option value="2">Shipped</option>';
-    }
-    if($row['status'] == 3){
-        $aa .= '<option selected value="3">Delivered</option>';
-    } else {
-        $aa .= '<option value="3">Delivered</option>';
-    }
-    if($row['status'] == 4){
         $aa .= '<option selected value="4">Completed</option>';
-    } else {
-        $aa .= '<option value="4">Completed</option>';
     }
     $aa .= '</select>';
     $sub_array[] = $aa;
     $eqURLID = base64_encode($row['id']);
-    $sub_array[] = '<div style="display: flex;"><a href="order-invoice?id='.$eqURLID.'" style="padding: 10px;" class="text-white btn btn-success fa fa-file view_order_details" type="button" id="'.$row['id'].'"></a>&nbsp;<a href="order-details?id='.$eqURLID.'" style="padding: 10px;" class="text-white btn btn-success fa fa-eye" type="button" id="'.$row['id'].'"></a></div>';
+    $sub_array[] = '<div style="display: flex;"><a href="order-invoice?id='.$eqURLID.'" style="padding: 10px;" class="text-white btn btn-success fa fa-file view_order_details" type="button" id="'.$row['id'].'"></a>&nbsp;<a href="order-details?id='.$eqURLID.'" style="padding: 10px;" class="text-white btn btn-info fa fa-eye" type="button" id="'.$row['id'].'"></a></div>';
     $data[] = $sub_array;
     $i++;
 }
