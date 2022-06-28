5e596aef937a175dfbcececb11f6ad39c607b892
Sanghani Chirag <csanghani58@gmail.com>
2022-06-28 19:48:52 +0530

    Updated 2022-06-28
    Some changs given by the client for NFT listing show all like image, gif, audio and video in listing

--- admin/resources/display_nft_list.php
+++ admin/resources/display_nft_list.php
@@ -9,10 +9,16 @@
 	foreach($result as $row)
 	{
 		$image = "";	
-		if(empty($row['image']) || $row['image_type'] != 'image'){
-			$image = '<img src="../images/planethopper-TV-logo.png" style = "border-radius: 40px" width="40" height="40" />';
-		} else {
+		if(!empty($row['image']) && $row['image_type'] == 'image'){
 			$image = '<img src="../images/nft_info_image/'.$row["image"].'" style = "border-radius: 40px" width="40" height="40" />';
+		} elseif (!empty($row['image']) && $row['image_type'] == 'gif') {
+			$image = '<img src="../images/nft_info_image/'.$row["image"].'" style = "border-radius: 40px" width="40" height="40" />';
+		} elseif (!empty($row['image']) && $row['image_type'] == 'video') {
+			$image = '<video width="100" height="100" controls><source src="../images/nft_info_image/'.$row['image'].'" type="video/mp4"></video>';
+		} elseif (!empty($row['image']) && $row['image_type'] == 'audio') {
+			$image = '<audio controls poster="../images/planethopper-TV-logo.png"><source src="../images/nft_info_image/'.$row['image'].'" type="audio/mpeg"></audio>';
+		} else {
+			$image = '<img src="../images/planethopper-TV-logo.png" style="border-radius: 40px" width="40" height="40" />';
 		}
 		$sub_array = array();
 		$sub_array[] = $i;
@@ -25,8 +31,9 @@
 		$sub_array[] = $row['assets_id'];
 		$sub_array[] = $row['meant_no'];
 		$sub_array[] = $row['duration'];
-		$sub_array[] = '<button class="btn btn-primary fa fa-pencil updateNFTInfo" data-toggle="modal" data-target="#updateNFTInfo" type="button" id="'.$row["id"].'"></button>';
-		$sub_array[] = '<button class="btn btn-danger fa fa-trash deleteNFTInfo" type="button" id="'.$row["id"].'" ></button>';
+		$sub_array[] = '<button class="btn btn-primary fa fa-pencil updateNFTInfo" data-toggle="modal"
+			data-target="#updateNFTInfo" type="button" id="'.$row["id"].'"></button>';
+		$sub_array[] = '<button class="btn btn-danger fa fa-trash deleteNFTInfo" type="button" id="'.$row["id"].'"></button>';
 		$data[] = $sub_array;
 		$i++;
 	}
